e0b9fb8ad344e42d28be078abfdf2481e6fc55b2
Kenneth Tuan - mac mini <kenneth9417@gmail.com>
2025-03-28 23:56:18 +0800

    chore: update base url for deployment

--- vite.config.mjs
+++ vite.config.mjs
@@ -5,7 +5,7 @@ import UnoCSS from "unocss/vite";
 
 export default ({ mode }) => {
   return defineConfig({
-    base: "/",
+    base: "/NBS-LMS",
     server: {
       port: 7001,
       cors: true,
